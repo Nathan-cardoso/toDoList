17921fc0d125a0e0b0e21938e9435e808a42f567
Nathan-cardoso <nathancar.linhares77@gmail.com>
2025-08-08 17:02:53 -0300

    feat: adicionando a pagina das task especificas

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,21 @@
 import { ChevronRight, Trash2} from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 function Tasks({ tasks, onTaskClick, onDeleteTask }) {
+
+    const navegation = useNavigate();
+    // Função para navegar para a página de detalhes da tarefa
+
+    function onSeeDetails(tasks){
+        const query = new URLSearchParams();
+        query.set("title", tasks.title);
+        query.set("description", tasks.description);
+        // Navega para a página de detalhes da tarefa com os parâmetros de consulta
+        // Isso permite que a página de detalhes acesse esses parâmetros através do hook useSearchParams
+
+        navegation(`/task?${query.toString()}`);
+    }
+
 
     return( 
         <ul className="space-y-2 bg-gray-50 rounded-md shadow-md">
@@ -15,7 +30,10 @@ function Tasks({ tasks, onTaskClick, onDeleteTask }) {
                         {tasks.title}
                     </button>
 
-                    <button className={`bg-blue-500 hover:bg-blue-600 p-2 m-3 rounded-md text-white ${tasks.isCompleted && 'bg-green-600 hover:bg-green-700 transition delay-50'}`}><ChevronRight/></button>
+                    <button className={`bg-blue-500 hover:bg-blue-600 p-2 m-3 rounded-md text-white ${tasks.isCompleted && 'bg-green-600 hover:bg-green-700 transition delay-50'}`}
+                            onClick={ () => onSeeDetails(tasks)}>
+                        <ChevronRight/>
+                    </button>
                     <button className="bg-red-600 p-2 m-3 rounded-md text-white cursor-pointer hover:bg-red-700 hover:transition delay-100 "
                             onClick={() => {onDeleteTask(tasks.id)}}>
                         <Trash2/>
--- src/components/pages/TaskPage.jsx
+++ src/components/pages/TaskPage.jsx
@@ -1,9 +1,45 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { ChevronLeft } from "lucide-react";
+
 function TaskPage(){
+    const navegate = useNavigate()
+    // O hook SearchParams permite acessar os parâmetros da URL
+    // Neste caso, estamos pegando os parâmetros "title" e "description"
+    const [ searchParams ] = useSearchParams();
+    const title = searchParams.get("title");
+    const description = searchParams.get("description");
+
+    // Renderiza a página com o título e descrição obtidos dos parâmetros
+
     return(
-        <div className="h-screen w-screen bg-[#f0f0f0] flex justify-center items-center">
-            <h1> Task page</h1>
+        <div className="h-screen w-screen bg-[#f0f0f0]">
+
+            <h1 className="font-bold text-4xl text-center p-4">Detalhes da Tarefa</h1>
+
+            <div className="text-center flex flex-col items-center justify-center">
+                <div className="bg-gray-50 w-[400px] h-[400px] rounded-2xl shadow-lg flex flex-col">
+
+                    <h2 className="font-bold text-3xl my-2 p-2 text-center">{title}</h2>
+
+                    <div className="flex flex-1 items-center justify-center">
+                        <p className="text-lg font-light text-center">{description}</p>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="flex justify-center">
+                <button 
+                    onClick={() => navegate("/")}
+                    className="bg-blue-500 rounded-2xl p-3 m-4  font-bold text-white cursor-pointer hover:bg-blue-600 flex"
+                    >
+                        < ChevronLeft size={30}  />
+                        Voltar
+                </button>
+            </div>
         </div>
+
     )
 }
 
-export default TaskPage;
+export default TaskPage;    
